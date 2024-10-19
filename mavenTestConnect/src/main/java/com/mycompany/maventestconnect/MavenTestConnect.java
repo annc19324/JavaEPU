3728abf1010826c2890baccd7d9255673caf5a22
annc19324 <annc19324@gmail.com>
2024-10-19 19:03:33 +0700

    update
    19/10/2024

---
+++ mavenTestConnect/src/main/java/com/mycompany/maventestconnect/MavenTestConnect.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.maventestconnect;
+
+/**
+ *
+ * @author annc1
+ */
+public class MavenTestConnect {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
